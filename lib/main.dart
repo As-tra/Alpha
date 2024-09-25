6a8ede0aab8c799524aee041f34f9462ebef48d3
Assil <astradhdbjd@gmail.com>
2024-09-25 13:26:45 +0100

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) {
+        return const Alpha();
+      },
+    ),
+  );
+}
+
+class Alpha extends StatelessWidget {
+  const Alpha({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      title: 'Flutter Demo',
+      home: const Placeholder(),
+    );
+  }
+}
